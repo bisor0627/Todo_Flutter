d44eb0e01b531fc847de88c3cf584a03b63d06dd
Jason <jason@twosun.com>
2022-01-19 18:20:46 +0900

    hi

--- todo_sqlite/lib/pages/insert.dart
+++ todo_sqlite/lib/pages/insert.dart
@@ -15,6 +15,7 @@ class InsertTodos extends StatefulWidget {
 }
 
 class _InsertTodoState extends State<InsertTodos> {
+// ! Variable
   late DatabaseHandler handler;
   late DateTime? _selectedTime;
 
@@ -26,10 +27,9 @@ class _InsertTodoState extends State<InsertTodos> {
 
   late StreamController<String> nameStreamController;
   late StreamController<String> descStreamController;
+  late StreamController<bool> btnStreamController;
 
-  late bool _isEnable;
-
-//Data
+// ! Actions
   Future<int> addTodo() async {
     Todos firstTodo = Todos(
         name: nameController.text,
@@ -39,19 +39,42 @@ class _InsertTodoState extends State<InsertTodos> {
     return await handler.insertTodos([firstTodo]);
   }
 
-//Widget
+  void controllerActions() {
+    nameStreamController = StreamController<String>.broadcast();
+    descStreamController = StreamController<String>.broadcast();
+    btnStreamController = StreamController<bool>.broadcast();
+
+    nameController.addListener(() {
+      nameStreamController.sink.add(nameController.text.trim());
+      btnStreamController.sink.add(descController.text.trim().isNotEmpty &&
+          nameController.text.trim().isNotEmpty);
+    });
+    descController.addListener(() {
+      descStreamController.sink.add(descController.text.trim());
+      btnStreamController.sink.add(descController.text.trim().isNotEmpty &&
+          nameController.text.trim().isNotEmpty);
+    });
+  }
+
+  void closeActions() {
+    nameStreamController.close();
+    descStreamController.close();
+    btnStreamController.close();
+    nameController.dispose();
+    descController.dispose();
+  }
+
+  void addTaskAction() async {
+    addTodo();
+    Navigator.pop(context);
+  }
 
   Color streamColor(Object? obj, FocusNode focus) {
     String text = obj.toString().trim();
     if (focus.hasFocus == true) {
       if (text.isEmpty) {
-        _isEnable = false;
         return Colors.redAccent;
       } else {
-        nameController.text.trim().isNotEmpty &&
-                descController.text.trim().isNotEmpty
-            ? _isEnable = true
-            : _isEnable = false;
         return Colors.blueAccent;
       }
     } else if (focus.hasFocus && text.isEmpty) {
@@ -61,32 +84,41 @@ class _InsertTodoState extends State<InsertTodos> {
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    handler = DatabaseHandler();
-    _selectedTime = DateTime.now();
-    nameStreamController = StreamController<String>.broadcast();
-    descStreamController = StreamController<String>.broadcast();
-
-    _isEnable = false;
-
-    nameController.addListener(() {
-      nameStreamController.sink.add(nameController.text.trim());
-    });
-
-    descController.addListener(() {
-      descStreamController.sink.add(descController.text.trim());
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    nameStreamController.close();
-    descStreamController.close();
-    nameController.dispose();
-    descController.dispose();
+// ! Widgets
+  List<Widget> _drawTitle() {
+    return [
+      Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 20, 16, 0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              'Add Task',
+              style: title2,
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: AutoSizeText(
+                'Fill out the details below to add a new task.',
+                style: bodyText1.override(
+                  fontFamily: 'Lexend Deca',
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14,
+                  color: tertiaryColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    ];
   }
 
   Widget _drawNameFIeld() {
@@ -138,6 +170,162 @@ class _InsertTodoState extends State<InsertTodos> {
     );
   }
 
+  Widget _drawDescField() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+      child: StreamBuilder(
+          stream: descStreamController.stream,
+          builder: (context, snapshot) {
+            return TextFormField(
+              controller: descController,
+              focusNode: descFocus,
+              textInputAction: TextInputAction.done,
+              onFieldSubmitted: (value) => addTaskAction,
+              decoration: InputDecoration(
+                  labelText: "Details", //'Details'
+                  labelStyle: bodyText1.override(
+                    fontFamily: 'Lexend Deca',
+                    color: tertiaryColor,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                  hintText:
+                      "Enter a description here...", //'Enter a description here...'
+                  hintStyle: bodyText1.override(
+                    fontFamily: 'Lexend Deca',
+                    color: tertiaryColor,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: streamColor(snapshot.data, descFocus),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: streamColor(snapshot.data, descFocus),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  )),
+              style: bodyText1,
+              textAlign: TextAlign.start,
+              maxLines: 3,
+            );
+          }),
+    );
+  }
+
+  Widget _drawDateBtn() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+      child: InkWell(
+        onTap: () async {},
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.92,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: black55,
+              width: 1,
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              Future<DateTime?> selectedDate = showDatePicker(
+                context: context,
+                initialDate: DateTime.now(), // 초깃값
+                firstDate: DateTime(2018), // 시작일
+                lastDate: DateTime(2030), // 마지막일
+              );
+
+              selectedDate.then((dateTime) {
+                setState(() {
+                  _selectedTime = dateTime;
+                });
+              });
+            },
+            child: Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+              child: Text(
+                DateFormat.yMMMd().format(_selectedTime ?? DateTime.now()),
+                style: bodyText1.override(
+                  fontFamily: 'Lexend Deca',
+                  color: tertiaryColor,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget drawBtnRow() {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            width: 130,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () async {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+              style: ElevatedButton.styleFrom(primary: Colors.grey),
+            ),
+          ),
+          SizedBox(
+            width: 130,
+            height: 50,
+            child: StreamBuilder<bool>(
+                stream: btnStreamController.stream,
+                builder: (context, snapshot) {
+                  // if (snapshot.connectionState == ConnectionState.none) {
+                  return ElevatedButton(
+                      onPressed: snapshot.data == null
+                          ? null
+                          : snapshot.data!
+                              ? () {
+                                  addTaskAction();
+                                }
+                              : null,
+                      child: const Text("Creat Task"));
+                  // } else {
+                  // return Text("no");
+                  // }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+// ! Builds
+  @override
+  void initState() {
+    super.initState();
+    handler = DatabaseHandler();
+    _selectedTime = DateTime.now();
+    controllerActions();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    closeActions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -149,168 +337,16 @@ class _InsertTodoState extends State<InsertTodos> {
           body: SingleChildScrollView(
             child: Column(
               mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 20, 16, 0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text(
-                        'Add Task',
-                        style: title2,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        child: AutoSizeText(
-                          'Fill out the details below to add a new task.',
-                          style: bodyText1.override(
-                            fontFamily: 'Lexend Deca',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 14,
-                            color: tertiaryColor,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                _drawNameFIeld(),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
-                  child: StreamBuilder(
-                      stream: descStreamController.stream,
-                      builder: (context, snapshot) {
-                        return TextFormField(
-                          controller: descController,
-                          focusNode: descFocus,
-                          textInputAction: TextInputAction.done,
-                          onFieldSubmitted: (value) => addTaskAction,
-                          decoration: InputDecoration(
-                              labelText: "Details", //'Details'
-                              labelStyle: bodyText1.override(
-                                fontFamily: 'Lexend Deca',
-                                color: tertiaryColor,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 14,
-                              ),
-                              hintText:
-                                  "Enter a description here...", //'Enter a description here...'
-                              hintStyle: bodyText1.override(
-                                fontFamily: 'Lexend Deca',
-                                color: tertiaryColor,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 14,
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: streamColor(snapshot.data, descFocus),
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: streamColor(snapshot.data, descFocus),
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              )),
-                          style: bodyText1,
-                          textAlign: TextAlign.start,
-                          maxLines: 3,
-                        );
-                      }),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
-                  child: InkWell(
-                    onTap: () async {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.92,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(
-                          color: black55,
-                          width: 1,
-                        ),
-                      ),
-                      child: GestureDetector(
-                        onTap: () {
-                          Future<DateTime?> selectedDate = showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(), // 초깃값
-                            firstDate: DateTime(2018), // 시작일
-                            lastDate: DateTime(2030), // 마지막일
-                          );
-
-                          selectedDate.then((dateTime) {
-                            setState(() {
-                              _selectedTime = dateTime;
-                            });
-                          });
-                        },
-                        child: Container(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              16, 16, 16, 0),
-                          child: Text(
-                            DateFormat.yMMMd()
-                                .format(_selectedTime ?? DateTime.now()),
-                            style: bodyText1.override(
-                              fontFamily: 'Lexend Deca',
-                              color: tertiaryColor,
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: 130,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("Cancel"),
-                          style: ElevatedButton.styleFrom(primary: Colors.grey),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 130,
-                        height: 50,
-                        child: ElevatedButton(
-                            onPressed: _isEnable ? addTaskAction : null,
-                            child: const Text("Creat Task")),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              children: _drawTitle() +
+                  [
+                    _drawNameFIeld(),
+                    _drawDescField(),
+                    _drawDateBtn(),
+                    drawBtnRow()
+                  ],
             ),
           )),
     );
-  }
-
-  void addTaskAction() async {
-    addTodo();
-    Navigator.pop(context);
   }
 } // _InsertTodoState
 
